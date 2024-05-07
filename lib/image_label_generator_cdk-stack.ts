d5bc218b1a5fbb9793291b4d685f1f71aaed68f7
sushmullur <mullur.sushruth@gmail.com>
2024-05-06 23:18:03 -0700

    Add S3 event-triggered lambda function to stack

--- lib/image_label_generator_cdk-stack.ts
+++ lib/image_label_generator_cdk-stack.ts
@@ -1,18 +1,24 @@
 import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { Bucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { Role, ServicePrincipal, ManagedPolicy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { Construct } from 'constructs';
 
+const PATH_TO_LAMBDA = '../ImageLabelGeneratorLambda/ImageProcessor.zip';
 
 export class ImageLabelGeneratorStack extends Stack {
-  constructor(scope: Construct, id: string, prefix: String, props?: StackProps) {
+  constructor(scope: Construct, id: string, prefix: string, props?: StackProps) {
     super(scope, id, props);
 
     // Bucket to store images
-    const bucket = new Bucket(this, `${prefix}-ImageBucket`, {
+    const inputBucket = new Bucket(this, `${prefix}-ImageBucket`, {
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true
     });
+
+    // Bucket to store labeled images
+    const outputBucket = new Bucket(this, `${prefix}-OutputBucket`);
 
     // Rekognition service role
     const rekognitionRole = new Role(this, `${prefix}-RekognitionServiceRole`, {
@@ -20,11 +26,41 @@ export class ImageLabelGeneratorStack extends Stack {
       managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AmazonRekognitionFullAccess')]
     });
 
-    bucket.grantRead(rekognitionRole);
+    // Lambda function to process images
+    const imageProcessor = new Function(this, `${prefix}-ImageProcessor`, {
+      runtime: Runtime.PYTHON_3_9,
+      code: Code.fromAsset(PATH_TO_LAMBDA),
+      handler: 'index.handler',
+      environment: {
+        OUTPUT_BUCKET: outputBucket.bucketName
+      }
+    });
 
-    // Output the bucket name
-    new CfnOutput(this, 'BucketName', {
-      value: bucket.bucketName
+    // Grant permissions to Lambda function
+    inputBucket.grantRead(imageProcessor);
+    outputBucket.grantWrite(imageProcessor);
+    imageProcessor.addToRolePolicy(new PolicyStatement({
+      actions: ['rekognition:DetectLabels'],
+      resources: ['*']
+    }));
+
+    // S3 event source for Lambda function
+    const fileTypes = ['.jpg', '.jpeg', '.png'];
+    fileTypes.forEach(suffix => {
+      imageProcessor.addEventSource(new S3EventSource(inputBucket, {
+        events: [EventType.OBJECT_CREATED_PUT],
+        filters: [{ suffix: suffix }]
+      }));
+    });
+
+    // Output bucket name
+    new CfnOutput(this, 'OutputBucketName', {
+      value: outputBucket.bucketName
+    });
+
+    // Output the input bucket name
+    new CfnOutput(this, 'InputBucketName', {
+      value: inputBucket.bucketName
     });
   }
 }
